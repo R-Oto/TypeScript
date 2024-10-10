b5e8beb323c68d9111c5c58bd760008b25c95d6b
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-10 22:23:07 +0400

    array & tupple

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 "use strict";
-function test(x) {
-    return x + 1;
-}
+let x;
+x = 2;
+//^^ same as let x: number = 2;
+console.log(typeof x);
+let y;
+y = 'oto';
+//arrays & tupples
+const arr = [1, 'name'];
+console.log(arr);
--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-function test(x: number): number  {
-    return x + 1
-}
+let x: number;
+x = 2;
+//^^ same as let x: number = 2;
+console.log(typeof x)
+
+let y: string;
+y = 'oto';
+
+//arrays & tupples
+const arr: [number, string] = [1, 'name']
+console.log(arr)
+
+const coords: [string, number, boolean, number][] = [
+    ['string', 1, true, 2],
+    ['string', -1, false, 4]
+]
