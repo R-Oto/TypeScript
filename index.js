e272d3109fc7e73f9b34b57cd9d64af2c756a492
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-12 12:14:46 +0400

    functions in ts

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict";
+function myfun(x, y) {
+    return x + y;
+}
+const result = myfun(1, 2);
+console.log(result);
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-let x: any = ''
+function myfun(x: number, y: number): number{
+    return x + y;
+}
+
+const result = myfun(1, 2)
+
+console.log(result)
