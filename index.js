4293b8083566abd062b9cd6ae05fd1b16420b6e6
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-12 12:02:51 +0400

    any

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 "use strict";
-let x;
-x = 2;
-//^^ same as let x: number = 2;
-console.log(typeof x);
-let y;
-y = 'oto';
-//arrays & tupples
-const arr = [1, 'name'];
-console.log(arr);
--- index.ts
+++ index.ts
@@ -1,16 +1,1 @@
-let x: number;
-x = 2;
-//^^ same as let x: number = 2;
-console.log(typeof x)
-
-let y: string;
-y = 'oto';
-
-//arrays & tupples
-const arr: [number, string] = [1, 'name']
-console.log(arr)
-
-const coords: [string, number, boolean, number][] = [
-    ['string', 1, true, 2],
-    ['string', -1, false, 4]
-]
+let x: any = ''
